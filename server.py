aec2a9735d0601c3f2b74de63ef1edfbd6f1af36
Sorta L⨀SŦ <77138706+v1s1t0r999@users.noreply.github.com>
2023-02-01 18:43:47 +0530

    strr?

--- server.py
+++ server.py
@@ -18,10 +18,10 @@ def index():
 
 @app.route("/join", methods=["GET"])
 def join():
-    display_name = request.args.get('display_name')
+    display_name = str(request.args.get('display_name'))
     mute_audio = request.args.get('mute_audio') # 1 or 0
     mute_video = request.args.get('mute_video') # 1 or 0
-    room_id = request.args.get('room_id')
+    room_id = str(request.args.get('room_id'))
     session[room_id] = {"name": display_name,
                         "mute_audio": mute_audio, "mute_video": mute_video}
     return render_template("join.html", room_id=room_id, display_name=session[room_id]["name"], mute_audio=session[room_id]["mute_audio"], mute_video=session[room_id]["mute_video"])
@@ -29,20 +29,20 @@ def join():
 
 @socketio.on("connect")
 def on_connect():
-    sid = request.sid
+    sid = str(request.sid)
     print("New socket connected ", sid)
 
 
 @socketio.on("join-room")
 def on_join_room(data):
-    sid = request.sid
-    room_id = data["room_id"]
-    display_name = session[room_id]["name"]
+    sid = str(request.sid)
+    room_id = str(data["room_id"])
+    display_name = str(session[room_id]["name"])
 
     # register sid to the room
     join_room(room_id)
-    rooms_sid[sid] = room_id
-    names_sid[sid] = display_name
+    rooms_sid[sid] = str(room_id)
+    names_sid[sid] = str(display_name)
 
     # broadcast to others in the room
     print("[{}] New member joined: {}<{}>".format(room_id, display_name, sid))
@@ -51,24 +51,22 @@ def on_join_room(data):
 
     # add to user list maintained on server
     if room_id not in users_in_room:
-        users_in_room[room_id] = [sid]
+        users_in_room[room_id] = str([sid])
         emit("user-list", {"my_id": sid})  # send own id only
     else:
-        usrlist = {u_id: names_sid[u_id]
-                   for u_id in users_in_room[room_id]}
+        usrlist = {u_id: names_sid[u_id] for u_id in users_in_room[room_id]}
         # send list of existing users to the new member
         emit("user-list", {"list": usrlist, "my_id": sid})
         # add new member to user list maintained on server
         users_in_room[room_id].append(sid)
-
     print("\nusers: ", users_in_room, "\n")
 
 
 @socketio.on("disconnect")
 def on_disconnect():
-    sid = request.sid
-    room_id = rooms_sid[sid]
-    display_name = names_sid[sid]
+    sid = str(request.sid)
+    room_id = str(rooms_sid[sid])
+    display_name = str(names_sid[sid])
 
     print("[{}] Member left: {}<{}>".format(room_id, display_name, sid))
     emit("user-disconnect", {"sid": sid},
@@ -86,14 +84,14 @@ def on_disconnect():
 
 @socketio.on("data")
 def on_data(data):
-    sender_sid = data['sender_id']
-    target_sid = data['target_id']
+    sender_sid = str(data['sender_id'])
+    target_sid = str(data['target_id'])
     if sender_sid != request.sid:
-        print("[Not supposed to happen!] request.sid and sender_id don't match!!!")
+        print("\n[Not supposed to happen!] request.sid and sender_id don't match!!!\n")
 
-    if data["type"] != "new-ice-candidate":
+    if str(data["type"]) != "new-ice-candidate":
         print('{} message from {} to {}'.format(
-            data["type"], sender_sid, target_sid))
+            str(data["type"]), sender_sid, target_sid))
     socketio.emit('data', data, room=target_sid)
 
 @app.errorhandler(500)
