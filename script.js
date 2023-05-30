857ebe8bcb2f4596df20dbe34abfe364f0141ad3
Sorta L⨀SŦ <77138706+v1s1t0r999@users.noreply.github.com>
2023-05-30 13:28:23 +0530

    nub

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
+
+
